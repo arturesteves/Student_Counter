10e614890a4539a6f968eeef530066f870614bfc
arturesteves <arturmesteves@hotmail.com>
2017-05-30 02:50:40 +0100

    Unit tests for Teacher and Student

--- Student_Counter/__tests__/student.crud.js
+++ Student_Counter/__tests__/student.crud.js
@@ -1,22 +1,54 @@
-/**
- * Created by artur on 30/05/2017.
- */
+
 const Student = require('../src/lib/Student');
 
 
 
-test('Create a student in database and check id after and before creation', () => {
+test('Create a student in database', () => {
     var student = new Student("1400000", "João José", "13-02-1990", "joao.jose@gmail.com", "myPhoto.png");
-    expect(student.id).toBeUndefined(); // check if id is null before inserting on database
     student.save();
-    expect(student).toHaveProperty("id");   // check if id is now defined
-    expect(typeof student.id).toBe("string");   // check if id is type of string
+    expect(student).toHaveProperty("number");
+    expect(student).toHaveProperty("name");
+    expect(student).toHaveProperty("birthDate");
+    expect(student).toHaveProperty("email");
+    expect(student).toHaveProperty("photo");
+    expect(Student.retrieve(student.number).number).resolves.toBe("1400000");
+    expect(Student.retrieve(student.number).name).resolves.toBe("João José");
+    expect(Student.retrieve(student.number).birthDate).resolves.toBe("13-02-1990");
+    expect(Student.retrieve(student.number).email).resolves.toBe("joao.jose@gmail.com");
+    expect(Student.retrieve(student.number).photo).resolves.toBe("myPhoto.png");
 });
 
-test('Update a student ', () => {
-    var student = new Student("1400000", "João José", "13-02-1990", "joao.jose@gmail.com", "myPhoto.png");
-    expect(student.id).toBeUndefined();
+test('Update a student', () => {
+    var student = new Student("1400001", "João José", "13-02-1990", "joao.jose@gmail.com", "myPhoto.png");
     student.save();
-    expect(student).toHaveProperty("id");
-    expect(typeof student.id).toBe("string");
+
+    student.number = "1500000";
+    student.name = "Pedro Manteigas";
+    student.birthDate = "14-02-2000";
+    student.email = "pedro.manteigas@gmail";
+    student.photo = "newPhoto.png";
+
+    student.save();
+    expect(Student.number).not.toBe("1400001");
+    expect(Student.name).not.toBe("João José");
+    expect(Student.birthDate).not.toBe("13-02-1990");
+    expect(Student.email).not.toBe("joao.jose@gmail.com");
+    expect(Student.photo).not.toBe("myPhoto.png");
+    expect(Student.retrieve(student.number).number).resolves.toBe("1500000");
+    expect(Student.retrieve(student.number).name).resolves.toBe("Pedro Manteigas");
+    expect(Student.retrieve(student.number).birthDate).resolves.toBe("14-02-2000");
+    expect(Student.retrieve(student.number).email).resolves.toBe("pedro.manteigas@gmail");
+    expect(Student.retrieve(student.number).photo).resolves.toBe("newPhoto.png");
+});
+
+test('Delete a student', () => {
+    var student = new Student("1400001", "João José", "13-02-1990", "joao.jose@gmail.com", "myPhoto.png");
+    student.save();
+    student.delete();
+
+    expect(Student.retrieve(student.number).number).resolves.not.toBe("1500000");
+    expect(Student.retrieve(student.number).name).resolves.toBeUndefined();
+    expect(Student.retrieve(student.number).birthDate).resolves.toBeUndefined();
+    expect(Student.retrieve(student.number).email).resolves.toBeUndefined();
+    expect(Student.retrieve(student.number).photo).resolves.toBeUndefined();
 });
---
+++ Student_Counter/__tests__/teacher.crud.js
@@ -0,0 +1,52 @@
+
+const Teacher = require('../src/lib/Teacher');
+
+
+
+test('Create a teacher in database and check id after and before creation', () => {
+    var teacher = new Teacher("Manuel Tremoço", "manuel.tremoco@gmail.com", "13-02-1800", "mYpHOTO.JPEG");
+    expect(teacher.id).toBeUndefined();
+    teacher.save();
+    expect(teacher).toHaveProperty("name");
+    expect(teacher).toHaveProperty("email");
+    expect(teacher).toHaveProperty("birthDate");
+    expect(teacher).toHaveProperty("photo");
+
+    expect(Teacher.retrieve(teacher.id).name).resolves.toBe("Manuel Tremoço");
+    expect(Teacher.retrieve(teacher.id).email).resolves.toBe("manuel.tremoco@gmail.com");
+    expect(Teacher.retrieve(teacher.id).birthDate).resolves.toBe("13-02-1800");
+    expect(Teacher.retrieve(teacher.id).photo).resolves.toBe("mYpHOTO.JPEG");
+});
+
+test('Update a teacher', () => {
+    var teacher = new Teacher("Manuel Tremoço", "manuel.tremoco@gmail.com", "13-02-1800", "mYpHOTO.JPEG");
+    teacher.save();
+
+    teacher.name = "Pedro Manteigas";
+    teacher.email = "pedro.manteigas@gmail";
+    teacher.birthDate = "14-02-2000";
+    teacher.photo = "newPhoto.png";
+
+    teacher.save();
+    expect(Teacher.number).not.toBe("1400001");
+    expect(Teacher.name).not.toBe("João José");
+    expect(Teacher.birthDate).not.toBe("13-02-1990");
+    expect(Teacher.email).not.toBe("joao.jose@gmail.com");
+    expect(Teacher.photo).not.toBe("myPhoto.png");
+    expect(Teacher.retrieve(teacher.id).name).resolves.toBe("Manuel Tremoço");
+    expect(Teacher.retrieve(teacher.id).birthDate).resolves.toBe("13-02-1800");
+    expect(Teacher.retrieve(teacher.id).email).resolves.toBe("manuel.tremoco@gmail.com");
+    expect(Teacher.retrieve(teacher.id).photo).resolves.toBe("mYpHOTO.JPEG");
+});
+
+test('Delete a teacher', () => {
+    var teacher = new Teacher("Manuel Tremoço", "manuel.tremoco@gmail.com", "13-02-1800", "mYpHOTO.JPEG");
+    teacher.save();
+    teacher.delete();
+
+    expect(Teacher.retrieve(teacher.id).name).resolves.not.toBe("Manuel Tremoço");
+    expect(Teacher.retrieve(teacher.id).email).resolves.toBeUndefined();
+    expect(Teacher.retrieve(teacher.id).birthDate).resolves.toBeUndefined();
+    expect(Teacher.retrieve(teacher.id).photo).resolves.toBeUndefined();
+
+});
